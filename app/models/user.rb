96d55bfc6dd4d048376d3b6b53cd6a440f30041e
Hafiz Alwan <hafizalwan@gmail.com>
2018-09-04 20:38:46 +0800

    Associated with activity model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
 
   validates :fullname, presence: true, length: {maximum: 15}
+
+  has_many :activities
 end
