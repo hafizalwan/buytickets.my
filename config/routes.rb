4e7ee0f19e6137d419e4c3915b16f231cd9827a2
Hafiz Alwan <hafizalwan@gmail.com>
2018-09-04 20:39:17 +0800

    Created activities controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'activities/index'
+  get 'activities/new'
+  get 'activities/create'
+  get 'activities/update'
+  get 'activities/show'
+  get 'activities/description'
+  get 'activities/photos'
+  get 'activities/location'
+  get 'activities/price'
   root 'pages#home'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
