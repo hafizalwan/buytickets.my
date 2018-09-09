647e1f08e6e7ad94a05ec6c91030a1b3e4da5e5b
Hafiz Alwan <hafizalwan@gmail.com>
2018-09-10 03:29:45 +0800

    Removed old activity's model and controller, copied create page to copy-version.html

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,4 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :activities, except: [:edit] do
-    member do
-      get 'listing'
-      get 'price'
-      get 'description'
-      get 'photos'
-      get 'location'
-    end
-  end
 end
