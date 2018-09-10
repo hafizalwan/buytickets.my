ab231590cca800db20e9cefeb7ac0d9b21adf4c3
Hafiz Alwan <hafizalwan@gmail.com>
2018-09-11 03:55:51 +0800

    Modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
-  get 'activities/index'
-  get 'activities/new'
-  get 'activities/create'
-  get 'activities/update'
-  get 'activities/show'
-  get 'activities/listing'
-  get 'activities/details'
-  get 'activities/photos'
-  get 'activities/location'
-  get 'activities/price'
   root 'pages#home'
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:show]
+  resources :activities, except: [:edit] do
+    member do
+      get 'listing'
+      get 'details'
+      get 'photos'
+      get 'location'
+      get 'price'
+    end
+  end
 
 end
