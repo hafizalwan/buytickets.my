c36052b8e5916ba64ccb0809e99d74056a20bf6c
Hafiz Alwan <hafizalwan@gmail.com>
2018-09-10 17:36:44 +0800

    Created activities controller

---
+++ app/controllers/activities_controller.rb
@@ -0,0 +1,28 @@
+class ActivitiesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def details
+  end
+
+  def photos
+  end
+
+  def location
+  end
+
+  def price
+  end
+end
---
+++ db/migrate/20180910092413_create_activities.rb
@@ -0,0 +1,29 @@
+class CreateActivities < ActiveRecord::Migration[5.2]
+  def change
+    create_table :activities do |t|
+      t.string :name
+      t.text :about
+      t.text :details_1
+      t.text :details_2
+      t.text :how_to_directions
+      t.text :how_to_seats
+      t.text :activity_info
+      t.text :refund_policies
+      t.string :location_map
+      t.string :location_title
+      t.integer :base_price
+      t.integer :my_adult_price
+      t.integer :my_child_price
+      t.integer :int_adult_price
+      t.integer :int_child_price
+      t.boolean :is_base_price
+      t.boolean :is_my_adult
+      t.boolean :is_my_child
+      t.boolean :is_int_adult
+      t.boolean :is_int_child
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
