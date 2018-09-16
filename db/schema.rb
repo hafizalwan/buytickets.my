01ca2766666238f2ab40162194f6e93cf8386c60
Hafiz Alwan <hafizalwan@gmail.com>
2018-09-16 21:14:49 +0800

    Created photo model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_16_095856) do
+ActiveRecord::Schema.define(version: 2018_09_16_104927) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -62,6 +62,13 @@ ActiveRecord::Schema.define(version: 2018_09_16_095856) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_activities_on_user_id"
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.integer "activity_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["activity_id"], name: "index_photos_on_activity_id"
   end
 
   create_table "users", force: :cascade do |t|
